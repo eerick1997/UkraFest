d6a1992257d3b37544e8e0731e821e6c171811bc
Lalo1017 <eduardoto1017@gmail.com>
2019-11-29 08:43:45 -0600

    cambios

--- src/backend/cargaDiccionario.cpp
+++ src/backend/cargaDiccionario.cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
+#include "../Libraries/Trie.h"
+
 using namespace std;
 
 int main(){
 
     ifstream inFile;
     string str1, str2, str3, str4;
+    Trie trie;
 
-    inFile.open("Words.txt");
+    inFile.open("diccionario.txt");
 
     if(!inFile){
         cout << "No se ha encontrado el diccionario\n";
         exit(1);
     }
-
     
     while( inFile >> str1 >> str2 >> str3 >> str4 ){
-        // cout << str2 << endl; No habilitar este comentario, son muchas palabras Xd
+        //cout << str2 << endl; //No habilitar este comentario, son muchas palabras Xd
         // Ingresar las palabras a la clase Trie
+        trie.insert_word(str2);
     }
-
     inFile.close();
 
     return 0;
