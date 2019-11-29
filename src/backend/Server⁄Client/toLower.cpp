18394c810d0e2cdc942d779fb53a6d6bed187207
Lalo1017 <eduardoto1017@gmail.com>
2019-11-29 11:27:18 -0600

    toLower.cpp

--- src/backend/Server⁄Client/toLower.cpp
+++ src/backend/Server⁄Client/toLower.cpp
@@ -1,51 +1,56 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    vector< string > words = { "aeiou", "AEIOU", "ÁÉÍÓÚ", ".-,+´" };
+    vector< string > words = { "aeiou", "AEIOU", "ÁÉÍÓÚ", "áéíóú", ".-,+´" };
     vector< string > newWords;
 
+    string cad_aux = "áéíóú";
+
     for( string word : words ){
+
         string newWord = "";
 
-        for( char c : word ){
-            if( isalpha( c ) )
-                newWord += tolower( c );
-            else {
-                cout << c;
-                switch ( c ) {
-                case 'Á':
-                    newWord += 'á';
-                break;
-                case 'É':
-                    newWord += 'é';
-                break;
-                case 'Í':
-                    newWord += 'í';
-                break;
-                case 'Ó':
-                    newWord += 'ó';
-                break;
-                case 'Ú':
-                    newWord += 'ú';
-                break;
-                case 'Ü':
-                    newWord += 'Ü';
-                break;
-                case 'Ñ':
-                    newWord += 'ñ';
-                break;
-                }
+        for(int i=0; i<word.length(); i++){
+
+            if( isalpha(word[i]) )
+                newWord += tolower(word[i]);
+
+            else{
+
+                int aux = (int)word[i];
+
+                if( aux == -61 )
+                    newWord += word[i];
+
+                else if ( aux == -127 || aux == -95 )
+                    newWord += cad_aux[1];
+
+                else if( aux ==  -119 || aux == -87 )
+                    newWord += cad_aux[3];
+
+                else if( aux ==  -115 || aux == -83 )
+                    newWord += cad_aux[5];
+
+                else if( aux ==  -109 || aux == -77 )
+                    newWord += cad_aux[7];
+
+                else if( aux ==  -102 || aux == -70 )
+                    newWord += cad_aux[9];
+
             }
-            cout << endl;
+
         }
-        if( newWord[ 0 ] != ' ' )
+
+        if( newWord.length() != 0)
             newWords.push_back( newWord );
-        newWord.clear();
     }
+
+    cout << endl << endl;
     for( string word : newWords )
         cout << word << endl;
 
